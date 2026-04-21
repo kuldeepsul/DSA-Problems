b01f0979044049251f7efbb34df315fc20bc13b4
kuldeepsul <kuldeepsul29@gmail.com>
2026-04-21 23:01:47 +0530

    Still Working on backtracking problems.

--- Backtracking.h
+++ Backtracking.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <map>
 #include <unordered_map>
+#include <algorithm>
 
 class Backtracking
 {
@@ -10,7 +11,7 @@ public:
     static std::vector<std::vector<int>> subsets(std::vector<int>& nums) {
         std::vector<std::vector<int>> res;
         std::vector<int> subset;
-        dfsrev(nums, 0, subset, res);
+        dfs(nums, 0, subset, res);
         return res;
     }
 
@@ -78,30 +79,32 @@ public:
 
     }
 
-    static void step2(const std::vector <int>& nums, const int& target, int i, int& sum, std::vector<std::vector<int>>& res, std::vector <int>& temp)
+    static void step2(const std::vector <int>& nums, int target, int i, std::vector<std::vector<int>>& res, std::vector <int>& temp)
     {
-        if (sum == target)
+        if (target == 0)
         {
             res.push_back(temp);
             return;
         }
-        else if (sum > target)
+        else if (target < 0)
         {
             return;
         }
-        if (i == nums.size())
+        if (i >= nums.size())
         {
-            i = 0;
             return;
         }
-
 
         temp.push_back(nums[i]);
-        sum = sum + temp.back();
-        step(nums, target, i + 1 , sum, res, temp);
-        sum = sum - temp.back();
+        target -= nums[i];
+        step2(nums, target, i + 1, res, temp);
+        target += temp.back();
         temp.pop_back();
-        step(nums, target, i + 1, sum, res, temp);
+        while (i + 1 < nums.size() && nums[i] == nums[i + 1])
+        {
+            i++;
+        }
+        step2(nums, target, i + 1 , res, temp);
 
 
     }
@@ -109,8 +112,8 @@ public:
     {
         std::vector <std::vector <int>> res;
         std::vector <int> temp;
-        int sum = 0;
-        step(nums, target, 0, sum, res, temp);
+        std::stable_sort(nums.begin(),nums.end());
+        step2(nums, target, 0, res, temp);
         return res;
 
     }
@@ -155,17 +158,223 @@ public:
         return result;
     }
 
-    static void permutationstep2(std::vector <std::vector<int>>& result, const std::vector<int> &nums)
+
+    static void perm(std::vector<std::vector<int>> &res, std::vector<int> temp ,int &sum,const int &target)
     {
+        if (sum > 0)
+        {
+            return;
+        }
+        if (sum < -target)
+        {
+            return;
+        }
+        if (temp.size() == 2 * target)
+        {
+            if (sum == 0)
+            {
+                res.push_back(temp);
+            }
+            return;
+        }
+
+        temp.push_back(-1);
+        sum -= 1;
+        perm(res, temp, sum, target);
+        temp.pop_back();
+        sum += 1;
+
+        temp.push_back(1);
+        sum += 1;
+        perm(res, temp, sum, target);
+        temp.pop_back();
+        sum -= 1;
 
     }
 
-
-    static std::vector<std::vector<int>> permute2(std::vector<int>& nums)
+    static std::vector<std::vector<int>> genparenthesis(int n)
     {
-        std::vector <std::vector<int>> result;;
-        permutationstep2(result, nums);
+        std::vector<std::vector<int>> res;
+        std::vector<int> temp;
+        int sum = 0;
+        perm(res,temp,sum,n);
 
-        return result;
+        return res;
+        
+    }
+
+    static void permstr(std::vector<std::string>& res, std::string &temp, int& sum, const int& target)
+    {
+        if (sum > 0)
+        {
+            return;
+        }
+        if (sum < -target)
+        {
+            return;
+        }
+        if (temp.size() == 2 * target)
+        {
+            if (sum == 0)
+            {
+                res.push_back(temp);
+            }
+            return;
+        }
+
+        temp.push_back('(');
+        sum -= 1;
+        permstr(res, temp, sum, target);
+        temp.pop_back();
+        sum += 1;
+
+        temp.push_back(')');
+        sum += 1;
+        permstr(res, temp, sum, target);
+        temp.pop_back();
+        sum -= 1;
+
+    }
+
+    static std::vector<std::string> genparenthesisstr(int n)
+    {
+        std::vector<std::string> res;
+        std::string temp;
+        int sum = 0;
+        permstr(res, temp, sum, n);
+
+        return res;
+
+    }
+    static bool searchnearby(std::vector<std::vector<char>>& board, std::string word, const int& i, const int& j , int n, std::map <std::pair<int, int>, int> &cur_path)
+    {
+        if (n >= word.size())
+        {
+            return true;
+        }
+
+        bool left = false;
+        bool right = false;
+        bool up = false;
+        bool down = false;
+        
+
+        if (i)
+        {
+            if (board[i - 1][j] == word[n])
+            {
+                auto it = cur_path.find({i-1,j});
+                if (it == cur_path.end())
+                {
+                    cur_path[{i - 1, j}]++;
+                    up = searchnearby(board, word, i - 1, j, n + 1,cur_path);
+                    cur_path.erase({ i - 1, j });
+                }
+            }
+        }
+        if (j)
+        {
+            if (board[i][j - 1] == word[n])
+            {
+                auto it = cur_path.find({i,j-1});
+                if (it == cur_path.end())
+                {
+                    cur_path[{i, j - 1}]++;
+                    left = searchnearby(board, word, i, j - 1, n + 1, cur_path);
+                    cur_path.erase({i,j-1});
+                }
+
+            }
+        }
+        if (i != board.size() - 1)
+        {
+            if (board[i + 1][j] == word[n])
+            {
+                auto it = cur_path.find({i+1,j});
+                if (it == cur_path.end())
+                {
+                    cur_path[{i + 1, j}]++;
+                    down = searchnearby(board, word, i + 1, j, n + 1,cur_path);
+                    cur_path.erase({i+1,j});
+                }
+            }
+        }
+        if (j != board[0].size() - 1)
+        {
+            if (board[i ][j + 1] == word[n])
+            {
+                auto it = cur_path.find({i,j+1});
+                if (it == cur_path.end())
+                {
+                    cur_path[{i, j + 1}]++;
+                    right = searchnearby(board, word, i, j + 1, n + 1,cur_path);
+                    cur_path.erase({i,j+1});
+                }  
+            }
+        }
+
+        return left || right || up || down;
+    }
+    
+    static bool search(std::vector<std::vector<char>>& board, std::string word,int &i , int &j)
+    {
+        std::map <std::pair<int,int>,int>cur_path;
+        for (int k = i; k < board.size(); k++)
+        {
+            for (int l = j; l < board[0].size(); l++)
+            {
+                bool cur = false;
+                if (board[k][l] == word[0])
+                {
+                    cur_path[{k,l}]++;
+                    cur = searchnearby(board, word, k, l, 1,cur_path);
+                    cur_path.clear();
+                }
+                if (cur)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static bool exist(std::vector<std::vector<char>>& board, std::string word) 
+    {
+        int i = 0;
+        int j = 0;
+        return search(board,word,i,j);
+    }
+
+    static void getcombinations(int i, int j, std::vector<std::string>& res, std::string& temp, const std::vector <std::string> &input)
+    {
+        for (int k = i; k < input.size(); k++)
+        {
+            for (int l = j; l < input[i].size(); l++)
+            {
+
+            }
+        }
+    }
+
+    static std::vector<std::string> LetterCombinations(std::string digit)
+    {
+        std::unordered_map <char,std::string> alphamap;
+        alphamap['2'] = "abc";
+        alphamap['3'] = "def";
+        alphamap['4'] = "ghi";
+        alphamap['5'] = "jkl";
+        alphamap['6'] = "mno";
+        alphamap['7'] = "pqrs";
+        alphamap['8'] = "tuv";
+        alphamap['9'] = "wxyz";
+        std::vector <std::string> res;
+        std::string temp;
+        std::vector <std::string> input;
+        for (auto it = alphamap.begin(); it != alphamap.end(); it++)
+        {
+            input.push_back(it->second);
+        }
+        getcombinations(0,0, res, temp, input);
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,79 +1,21 @@
 #include <iostream>
-#include "Tree.h"
-#include "Linked_List.h"
+#include <string>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <algorithm>
 #include "Backtracking.h"
-#include "Array.h"
-
-
-//class Base
-//{
-//public:
-//	virtual void function1()
-//	{
-//		std::cout << "Function from Base Class" << std::endl;
-//	}
-//	void function2()
-//	{
-//		std::cout << "Function2 from Base Class" << std::endl;
-//	}
-//};
-//
-//class Derived : public Base
-//{
-//public:
-//	void function1()
-//	{
-//		std::cout << "Function from Derived1 Class" << std::endl;
-//	}
-//	
-//};
-//
-//class Derived2 : public Base
-//{
-//public:
-//	void function3()
-//	{
-//		std::cout << "Function3 from Derived2 class" << std::endl;
-//	}
-//
-//};
-//
-//
-//int main(void) 
-//{
-//	Base* pbase = new Base();
-//	Derived* pderived1 = new Derived();
-//	Derived2* pderived2 = new Derived2();
-//
-//	pbase->function1();
-//	pderived1->function1();
-//	pderived2->function1();
-//
-//	Base* pbase1 = new Derived();
-//	Base* pbase2 = new Derived2();
-//
-//	std::cout << "Using Base Pointer and creating derived object instance on it." << std::endl;
-//	pbase1->function1();
-//	pbase2->function1();
-//
-//
-//	std::cout << "Using Base pointer and derived pointer , and calling function2" << std::endl;
-//	pbase1->function2();
-//	pderived1->function2();
-//	pderived2->function2();
-//
-//
-//	std::cout << "Using Function 3 only present in Derived2 , once from a base pointer and after from its own pointer." << std::endl;
-//	pderived2->function3();
-//	pbase2->function3();
-//
-//	return 0;
-//}
 
 int main()
 {
-	std::vector <int> nums = { 1,2,3 };
-
-	std::vector <std::vector<int>> res = Backtracking::permute(nums);
+	std::vector <int> data = { 9,2,2,4,6,1,5 };
+	std::vector<std::vector<char>> board = {
+		{'A','B','C','E'},
+		{'S','F','C','S'},
+		{'A','D','E','E'}
+	};
+	
+	bool res = Backtracking::exist(board,"ABCD");
 	return 0;
-}
+};
